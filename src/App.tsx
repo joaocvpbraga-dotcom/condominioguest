8f32e1d0a8e03d002d9422682515884be9c9637c
João Braga <joaocvpbraga@gmail.com>
2026-04-27 16:54:23 +0100

    fix: corrigido uso do BrowserRouter para build e deploy

--- src/App.tsx
+++ src/App.tsx
@@ -16,31 +16,33 @@ import { ManutencoesPage } from '@/pages/ManutencoesPage'
 
 function App() {
   return (
-    <AuthProvider>
-      <AppDataProvider>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><DashboardPage /></RoleRoute>} />
-            <Route path="moradores" element={<RoleRoute allowedRoles={['admin']}><MoradoresPage /></RoleRoute>} />
-            <Route path="quotas" element={<RoleRoute allowedRoles={['admin', 'morador']}><QuotasPage /></RoleRoute>} />
-            <Route path="ocorrencias" element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><OcorrenciasPage /></RoleRoute>} />
-            <Route path="comunicados" element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><ComunicadosPage /></RoleRoute>} />
-            <Route path="documentos" element={<RoleRoute allowedRoles={['admin', 'morador']}><DocumentosPage /></RoleRoute>} />
-            <Route path="contabilidade" element={<RoleRoute allowedRoles={['admin', 'morador']}><ContabilidadePage /></RoleRoute>} />
-            <Route path="manutencoes" element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><ManutencoesPage /></RoleRoute>} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AppDataProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <AppDataProvider>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><DashboardPage /></RoleRoute>} />
+              <Route path="moradores" element={<RoleRoute allowedRoles={['admin']}><MoradoresPage /></RoleRoute>} />
+              <Route path="quotas" element={<RoleRoute allowedRoles={['admin', 'morador']}><QuotasPage /></RoleRoute>} />
+              <Route path="ocorrencias" element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><OcorrenciasPage /></RoleRoute>} />
+              <Route path="comunicados" element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><ComunicadosPage /></RoleRoute>} />
+              <Route path="documentos" element={<RoleRoute allowedRoles={['admin', 'morador']}><DocumentosPage /></RoleRoute>} />
+              <Route path="contabilidade" element={<RoleRoute allowedRoles={['admin', 'morador']}><ContabilidadePage /></RoleRoute>} />
+              <Route path="manutencoes" element={<RoleRoute allowedRoles={['admin', 'morador', 'inquilino']}><ManutencoesPage /></RoleRoute>} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </AppDataProvider>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
